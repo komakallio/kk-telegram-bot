86291cf69afc7241ec6195ac3029d27b9546abff
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2017-06-30 21:58:33 +0300

    Add more weather API tests.

--- test/api.test.js
+++ test/api.test.js
@@ -17,5 +17,32 @@ describe('kk-api', () => {
       kk_api.weather();
       assert(this.requestStub.calledWithMatch({url: 'http://komakallio.dy.fi:9001/weather'}));
     });
+
+    it('should call success_cb on success', () => {
+      this.requestStub.callsArgWith(1, undefined, {statusCode: 200}, {});
+      let success_cb = sinon.spy();
+      let failure_cb = sinon.spy();
+      kk_api.weather(success_cb, failure_cb);
+      assert(success_cb.calledOnce);
+      assert(failure_cb.notCalled);
+    });
+
+    it('should call failure_cb on wrong status code', () => {
+      this.requestStub.callsArgWith(1, undefined, {statusCode: 404}, {});
+      let success_cb = sinon.spy();
+      let failure_cb = sinon.spy();
+      kk_api.weather(success_cb, failure_cb);
+      assert(success_cb.notCalled);
+      assert(failure_cb.calledOnce);
+    });
+
+    it('should call failure_cb on error', () => {
+      this.requestStub.callsArgWith(1, new Error('Error!'), {statusCode: 200}, {});
+      let success_cb = sinon.spy();
+      let failure_cb = sinon.spy();
+      kk_api.weather(success_cb, failure_cb);
+      assert(success_cb.notCalled);
+      assert(failure_cb.calledOnce);
+    });
   });
 });
