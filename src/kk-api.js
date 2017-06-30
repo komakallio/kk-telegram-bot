356b53b57177cbf5ff6d71f58a651e8c93685b90
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2017-06-30 23:54:54 +0300

    Add rain endpoint to API.

--- src/kk-api.js
+++ src/kk-api.js
@@ -8,6 +8,7 @@ exports.weather = function(success_cb, failure_cb) {
     url: api_url + 'weather',
     json: true
   };
+
   request.get(options, function(err, res, body) {
     if (err) {
       winston.log('error', 'Weather API error: ' + err);
@@ -21,3 +22,23 @@ exports.weather = function(success_cb, failure_cb) {
     success_cb(body);
   });
 };
+
+exports.rain = function(success_cb, failure_cb) {
+  let options = {
+    url: api_url + 'rain',
+    json: true
+  };
+
+  request.get(options, function(err, res, body) {
+    if (err) {
+      winston.log('error', 'Rain API error: ' + err);
+      failure_cb();
+      return;
+    } else if (res.statusCode != 200) {
+      winston.log('error', 'Rain API returned status code: ' + res.statusCode);
+      failure_cb();
+      return;
+    }
+    success_cb(body);
+  });
+}
--- test/kk-api.test.js
+++ test/kk-api.test.js
@@ -57,4 +57,54 @@ describe('kk-api', () => {
       assert(failure_cb.calledOnce);
     });
   });
+
+  describe('#rain()', () => {
+    beforeEach(() => {
+      // Stub request.get(...)
+      this.requestStub = sinon.stub(request, 'get');
+    });
+
+    afterEach(() => {
+      this.requestStub.restore();
+    });
+
+    it('should call correct url', () => {
+      kk_api.rain();
+
+      assert(this.requestStub.calledWithMatch({url: 'http://komakallio.dy.fi:9001/rain'}));
+    });
+
+    it('should call success_cb on success', () => {
+      this.requestStub.callsArgWith(1, undefined, {statusCode: 200}, {});
+      let success_cb = sinon.spy();
+      let failure_cb = sinon.spy();
+
+      kk_api.rain(success_cb, failure_cb);
+
+      assert(success_cb.calledOnce);
+      assert(failure_cb.notCalled);
+    });
+
+    it('should call failure_cb on wrong status code', () => {
+      this.requestStub.callsArgWith(1, undefined, {statusCode: 404}, {});
+      let success_cb = sinon.spy();
+      let failure_cb = sinon.spy();
+
+      kk_api.rain(success_cb, failure_cb);
+
+      assert(success_cb.notCalled);
+      assert(failure_cb.calledOnce);
+    });
+
+    it('should call failure_cb on error', () => {
+      this.requestStub.callsArgWith(1, new Error('Error!'), {statusCode: 200}, {});
+      let success_cb = sinon.spy();
+      let failure_cb = sinon.spy();
+
+      kk_api.rain(success_cb, failure_cb);
+
+      assert(success_cb.notCalled);
+      assert(failure_cb.calledOnce);
+    });
+  });
 });
