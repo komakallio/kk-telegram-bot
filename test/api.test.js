c67a1a7f2961eb316369e170b6fc3443e229ceca
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2017-06-30 22:04:27 +0300

    Disable printing logs to screen during tests.

--- test/api.test.js
+++ test/api.test.js
@@ -1,11 +1,16 @@
 const kk_api = require('../src/kk-api.js');
 const assert = require('assert');
 const sinon = require('sinon');
+const winston = require('winston');
 let request = require('request');
+
+// Disable logging for tests
+winston.remove(winston.transports.Console);
 
 describe('kk-api', () => {
   describe('#weather()', () => {
     beforeEach(() => {
+      // Stub request.get(...)
       this.requestStub = sinon.stub(request, 'get');
     });
 
@@ -15,6 +20,7 @@ describe('kk-api', () => {
 
     it('should call correct url', () => {
       kk_api.weather();
+
       assert(this.requestStub.calledWithMatch({url: 'http://komakallio.dy.fi:9001/weather'}));
     });
 
@@ -22,7 +28,9 @@ describe('kk-api', () => {
       this.requestStub.callsArgWith(1, undefined, {statusCode: 200}, {});
       let success_cb = sinon.spy();
       let failure_cb = sinon.spy();
+
       kk_api.weather(success_cb, failure_cb);
+
       assert(success_cb.calledOnce);
       assert(failure_cb.notCalled);
     });
@@ -31,7 +39,9 @@ describe('kk-api', () => {
       this.requestStub.callsArgWith(1, undefined, {statusCode: 404}, {});
       let success_cb = sinon.spy();
       let failure_cb = sinon.spy();
+
       kk_api.weather(success_cb, failure_cb);
+
       assert(success_cb.notCalled);
       assert(failure_cb.calledOnce);
     });
@@ -40,7 +50,9 @@ describe('kk-api', () => {
       this.requestStub.callsArgWith(1, new Error('Error!'), {statusCode: 200}, {});
       let success_cb = sinon.spy();
       let failure_cb = sinon.spy();
+
       kk_api.weather(success_cb, failure_cb);
+
       assert(success_cb.notCalled);
       assert(failure_cb.calledOnce);
     });
