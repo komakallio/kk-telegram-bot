556b35d60d633e40d2db2242b4c7d43dd469af62
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2017-07-11 15:23:56 +0300

    Extract redundant API stuff to one function.

--- src/api.js
+++ src/api.js
@@ -5,48 +5,28 @@ const api_utils = require('./api-utils.js');
 api_url = 'http://komakallio.dy.fi:9001/'
 
 exports.weather = function() {
-  return new Promise((resolve, reject) => {
-    let options = {
-      url: api_url + 'weather',
-      json: true
-    };
-
-    rp.get(options)
-      .then((body) => resolve(api_utils.parse_weather_data(body)))
-      .catch((err) => {
-        winston.log('error', 'Weather API error: ' + err);
-        reject();
-      });
-  });
+  return call_api('weather', api_utils.parse_weather_data, 'Weather');
 };
 
 exports.rain = function() {
-  return new Promise((resolve, reject) => {
-    let options = {
-      url: api_url + 'rain',
-      json: true
-    };
-
-    rp.get(options)
-      .then((body) => resolve(api_utils.parse_rain_data(body)))
-      .catch((err) => {
-        winston.log('error', 'Rain API error: ' + err);
-        reject();
-      });
-  });
+  return call_api('rain', api_utils.parse_rain_data, 'Rain');
 };
 
 exports.rain_trigger = function() {
+  return call_api('raintrigger', api_utils.parse_rain_trigger_data, 'Rain trigger');
+};
+
+let call_api = function(suffix, callback, name) {
   return new Promise((resolve, reject) => {
     let options = {
-      url: api_url + 'raintrigger',
+      url: api_url + suffix,
       json: true
-    };
+    }
 
     rp.get(options)
-      .then((body) => resolve(api_utils.parse_rain_trigger_data(body)))
+      .then((body) => resolve(callback(body)))
       .catch((err) => {
-        winston.log('error', 'Rain trigger API error: ' + err);
+        winston.log('error', name + ' API error: ' + err);
         reject();
       });
   });
